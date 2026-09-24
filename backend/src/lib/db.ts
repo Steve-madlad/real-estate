6ba4a694c9de675f7840fafc4806919eabab299a
Estifanos Wendimu <teffsauce2@gmail.com>
2026-09-24 17:51:38 +0300

    chore: added local compatible version for prisma instance

--- backend/src/lib/db.ts
+++ backend/src/lib/db.ts
@@ -1,19 +1,8 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
-import fs from "fs";
 import { PrismaClient } from "../../prisma/generated/client.js";
 
-// Load AWS RDS Root Certificate
-const rdsCa = fs.readFileSync("/root/certs/global-bundle.pem").toString();
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: true, // Strict validation against AWS CA
-    ca: rdsCa,
-  },
-});
-
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 const adapter = new PrismaPg(pool);
 
 export const prisma = new PrismaClient({ adapter });
